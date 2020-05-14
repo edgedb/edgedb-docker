88e4e9c4459f911a812c7fd4ff3608918e32ee66
David Golembiowski <dmgolembiowski@gmail.com>
2020-05-14 00:47:05 -0400

    new approach

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-/bin/bash /srv/edbpool-server/devsetup.sh
+
+#!/bin/bash
+
+echo -e "\e[30;48;5;82mEDBPool E-Z Setup\e[0m"
+
+if [[ $(id -u) -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+echo -e "\e[40;38;5;82mBeginning RPC/Reverse Shell Pipeline:"
+sudo -u edbpool -H bash -c "cd /home/edbpool/edbpool; bash docker-mock-scripts/setup.sh"
